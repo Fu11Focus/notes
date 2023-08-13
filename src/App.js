31a006a133739653fde293f7cb87793c660bce6a
Евгений Матрос <mr@MacBook-Air-Evgenij.local>
2023-08-13 16:04:12 +0300

    change themes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
+import { faMoon } from '@fortawesome/free-solid-svg-icons';
+import { faSun } from '@fortawesome/free-solid-svg-icons';
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      theme: 'dark',
+    };
+    this.changeTheme = this.changeTheme.bind(this);
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  changeTheme(e) {
+    if (this.state.theme === 'dark') {
+      this.setState({ theme: 'light' });
+    } else {
+      this.setState({ theme: 'dark' });
+    }
+    console.log(this.state.theme);
+  }
+
+  render() {
+    let btnTheme, atributeName;
+    if (this.state.theme === 'dark') {
+      btnTheme = <FontAwesomeIcon icon={faMoon} />;
+      document.body.setAttribute('theme', 'dark');
+    } else {
+      btnTheme = <FontAwesomeIcon icon={faSun} />
+      document.body.setAttribute('theme', 'light');
+    }
+
+    return (
+      <div className="App">
+        <button className='btnTheme' onClick={this.changeTheme}>{btnTheme}</button>
+        <article className='container'>
+          <h1 className='pageName'>My notes</h1>
+          <input type='text' />
+          <button><FontAwesomeIcon icon={faPlus} /></button>
+        </article>
+      </div>)
+  }
 }
+
 
 export default App;
